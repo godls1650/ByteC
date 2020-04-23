30e64f570edce7acf21af54cd33c83e707f1277a
godls1650 <55517456+godls1650@users.noreply.github.com>
2020-04-23 14:24:05 +0900

    Create MainApp_Control.c

---
+++ 재직자/0422/MainApp_Control.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/*
+프로그램에 
+
+1.			   2         3.        4.        5.
+*	  			    *     *		        *	      *	  *
+**				   **      *		   *	       * * 
+***				  ***       *		  *		        *
+****			 ****        *	     *		       * *
+*****			*****         *	    *		      *   *
+
+각각 1 ~ 5번의 모양으로 출력하세요
+단 
+printf()는  
+printf("*");  printf(" ");  printf("\n");   3가지만 사용합니다.
+
+
+*/
